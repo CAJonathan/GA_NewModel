be1ab98375b945ef2e064f67e9d9fdbee60ed1bd
cajonathan <dns140198@gmail.com>
2019-09-28 15:54:22 +0700

    do-something

--- src/main/java/com/hust/msolab/newmodel/GA/Utilities/Factors.java
+++ src/main/java/com/hust/msolab/newmodel/GA/Utilities/Factors.java
@@ -46,8 +46,8 @@ public class Factors {
     public static int GA_CHOSEN_PARENTS_SELECTION_FUNCTION = 3;     // values: 1, 2, 3
 
     // Choosing data file for all data experiment
-    public static String INPUT_FOLDER = "/home/dongocson/Documents/Work/Work/Document/LearningProgram/Project2/GA_NewModel/src/main/resources/Data7/";
-    public static String OUTPUT_FOLDER = "/home/dongocson/Documents/Work/Work/Document/LearningProgram/Project2/GA_NewModel/src/main/resources/Result7/";
+    public static String INPUT_FOLDER = "/home/dongocson/Documents/Work/Work/Document/LearningProgram/Project2/GA_NewModel/src/main/resources/Data/DataPlus/";
+    public static String OUTPUT_FOLDER = "/home/dongocson/Documents/Work/Work/Document/LearningProgram/Project2/GA_NewModel/src/main/resources/ResultPlusV=15/";
 
     // Choosing data file for single experiment
     public static String INPUT_FILE_PATH = "/home/dongocson/Documents/Work/Work/Document/LearningProgram/Project2/GA_NewModel/src/main/resources/he-n20-far.txt";
@@ -66,7 +66,7 @@ public class Factors {
     // ==================================================Never make any change unless you know exactly what you're going to do==================================================
 
     // Fixed factors in WRSN
-    public static double WCE_V = 5;
+    public static double WCE_V = 15;
     public static double WCE_U = 5;
     public static double WCE_P_MOVE = 1;
     public static double WCE_Emc = 80000;
