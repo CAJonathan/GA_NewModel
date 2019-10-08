1e418349ef81b806028934283923461d1e5582ea
cajonathan <dns140198@gmail.com>
2019-10-08 23:33:25 +0700

    do-something

--- src/main/java/com/hust/msolab/newmodel/GA/Utilities/Factors.java
+++ src/main/java/com/hust/msolab/newmodel/GA/Utilities/Factors.java
@@ -46,8 +46,8 @@ public class Factors {
     public static int GA_CHOSEN_PARENTS_SELECTION_FUNCTION = 3;     // values: 1, 2, 3
 
     // Choosing data file for all data experiment
-    public static String INPUT_FOLDER = "/home/dongocson/Documents/Work/Work/Document/LearningProgram/Project2/GA_NewModel/src/main/resources/DataICC/Sen4";
-    public static String OUTPUT_FOLDER = "/home/dongocson/Documents/Work/Work/Document/LearningProgram/Project2/GA_NewModel/src/main/resources/ResultICC/Sen4";
+    public static String INPUT_FOLDER = "/home/dongocson/Documents/Work/Work/Document/LearningProgram/Project2/GA_NewModel/src/main/resources/DataICC/Sen3";
+    public static String OUTPUT_FOLDER = "/home/dongocson/Documents/Work/Work/Document/LearningProgram/Project2/GA_NewModel/src/main/resources/ResultICC/Sen3";
 
     // Choosing data file for single experiment
     public static String INPUT_FILE_PATH = "/home/dongocson/Documents/Work/Work/Document/LearningProgram/Project2/GA_NewModel/src/main/resources/he-n20-far.txt";
